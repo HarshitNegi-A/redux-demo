7aa1837153ca93fd3804900076f529e15e6d97f4
HarshitNegi-A <harshitnegi226@gmail.com>
2024-11-06 20:37:50 +0530

    incrementBy2

--- redux-demo.js
+++ redux-demo.js
@@ -2,13 +2,13 @@ const redux=require('redux');
 
 
 const counterReducer=(state = { counter : 0},action)=>{
-    if (action.type === 'increment') {
+    if (action.type === 'incrementBy2') {
         return {
-          counter: state.counter + 1
+          counter: state.counter + 2
         };
-      } else if (action.type === 'decrement') {
+      } else if (action.type === 'decrementBy2') {
         return {
-          counter: state.counter - 1
+          counter: state.counter - 2
         };
       }
 }
@@ -25,8 +25,6 @@ const counterSubscriber=()=>{
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({type : 'increment'})
-store.dispatch({type : 'increment'})
-store.dispatch({type : 'increment'})
-store.dispatch({type : 'increment'})
-store.dispatch({type : 'decrement'})
+store.dispatch({type : 'incrementBy2'})
+store.dispatch({type : 'incrementBy2'})
+store.dispatch({type : 'decrementBy2'})
